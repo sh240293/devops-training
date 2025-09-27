6fce2e31084919427dc3152da12bd47612b9da79
Shanish Mohan <shanish.mohan@ril.com>
2025-09-27 16:10:11 +0530

    pytest

--- python-proj/Weather-App/weather_api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="home"),
-    path("result", views.result, name="result"),
-    # path('social_links', views.social_links),
-]
