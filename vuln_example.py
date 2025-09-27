9b90e563cdab861341b0b358230edaef4c2366f3
Shanish Mohan <shanish.mohan@ril.com>
2025-09-28 00:27:48 +0530

    added vuln code

--- vuln_example.py
+++ vuln_example.py
@@ -1,6 +1,8 @@
+from app import app
 from flask import request
-@app.route("/search")
-def search():
-    query = request.args.get("q")
-    return f"Searching for {query}"
 
+@app.route("/vuln_xss")
+def vuln_xss():
+    query = request.args.get("q", "")
+    # Vulnerable: directly embedding user input in HTML response
+    return f"<html><body>Searching for {query}</body></html>"
